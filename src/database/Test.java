4094e22d10336bfcad72ebaae251e5538cae2961
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-10-29 06:28:01 +0700

    Update Test.java

--- src/database/Test.java
+++ src/database/Test.java
@@ -1,5 +1,26 @@
 package database;
 
-public class Test {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Test 
+{
+	
+	
+	public static void main (String args[])
+	{
+		Connection conn = null;
+		try {
+		    conn =
+		       DriverManager.getConnection("jdbc:mysql://localhost:3306/test?" +
+		                                   "user=root&password=");
+		    System.out.print("Connected");
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}
 }
